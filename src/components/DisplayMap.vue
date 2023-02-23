3ae058ad0947b37288a2fd70f7c52fe594d61622
Jean Alvarez <jalvarez@octipas.com>
2023-02-23 22:19:48 +0100

    add rework

--- src/components/DisplayMap.vue
+++ src/components/DisplayMap.vue
@@ -2,7 +2,10 @@
   <div id="map">
   </div>
 
-  <h1>TROUVE TON CLUB!</h1>
+  <div class="header">
+    <img src="../assets/LOGO_BLASON_FEDE_CHESSBOXING_BLANC.png"/>
+    <h1>TROUVE TON CLUB!</h1>
+  </div>
 </template>
 
 <script>
@@ -18,6 +21,8 @@ export default {
       const map = new window.google.maps.Map(document.getElementById('map'), {
         zoom: 6,
         center: { lat: 46.2276, lng: 2.2137 },
+        zoomControl: false,
+        gestureHandling: "none",
         styles: [
           {
             "elementType": "geometry",
@@ -264,15 +269,21 @@ export default {
         console.log(city.lat)
         const marker = new window.google.maps.Marker({
           position: { lat: city.lat, lng: city.lng },
+          icon: '../src/assets/sm_gant.png',
           map,
         });
 
         const content = `
+        <div class="modal">
+        <img src="../src/assets/echecs_mat.png"/>
+        <div class="info">
           <h3>${ city.name }</h3>
-          <p>Address: ${ city.address }</p>
-          <p>Phone: ${ city.phone }</p>
-          <p
-          <a target="_blank" href="${ city.website }">Site Web</p>
+          <p>  <img class="icon" src="../src/assets/map.png"/><span>${ city.address }</span></p>
+          <a href="tel:${ city.phone }">  <img class="icon" src="../src/assets/phone.png"/> <span>${ city.phone }</span></a>
+
+          <a target="_blank" href="${ city.website }"> <img class="icon" src="../src/assets/web.png"/> <span>Site Web</span></a>
+          </div>
+          </div>
         `;
 
         const infowindow = new window.google.maps.InfoWindow({
@@ -308,26 +319,102 @@ export default {
   },
 };
 </script>
-<style>
+<style scss>
 #map {
   width: 100%;
   height: 100vh;
-  position: initial!important;
+  position: initial !important;
   text-transform: uppercase;
 }
 
-h1{
-  font-family: "Lato", sans-serif;
-  font-weight: 700;
-  font-style: italic;
-  position: absolute;
-  top: 0;
-  color: white;
-  font-size: 45px;
+
+img {
+  width: 10px;
 }
 
 
+.header {
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 20px;
+  align-items: center;
+  left: 25%;
+  width: 50%;
+}
+
+h1 {
+  font-family: "Lato", sans-serif;
+  font-weight: 700;
+  font-style: italic;
+  color: white;
+  font-size: 45px;
+  transform: rotate(-8deg);
+
+}
+
+img {
+  width: 150px;
+}
+
+a {
+  color: white;
+  font-family: "Lato", sans-serif;margin-bottom: 10px;
+}
+
+a:hover {
+  color: white;
+}
+
+.modal {
+  font-family: "Lato", sans-serif;
+  color: white;
+  text-align: left;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 428px;
+  height: 120px;
+  flex-direction: row;
+}
+
+span{
+  margin-left:5px;
+}
+
+p, a{
+  display: flex;
+}
+
+.icon {
+  width: 15px;
+}
+
+
+h3 {
+  margin-top: 0;
+  font-size: 22px;
+  font-style: italic;
+}
+
 .gmnoprint, .gm-control-active {
+  display: none;
+}
+
+.gm-style-iw-d {
+  background: none !important;
+  overflow: initial !important;
+}
+
+.gm-style .gm-style-iw-c {
+  border-radius: 3px !important;
+  padding: 30px !important;
+  top: 300px;
+  background: rgb(178, 36, 67);
+  background: linear-gradient(90deg, rgba(178, 36, 67, 1) 0%, rgba(20, 66, 104, 1) 100%, rgba(0, 212, 255, 1) 100%);
+}
+
+.gm-style-iw-tc::after {
   display: none;
 }
 </style>
